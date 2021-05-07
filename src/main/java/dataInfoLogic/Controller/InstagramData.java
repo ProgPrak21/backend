2df809ea66141cf13013ecf4bfe8aecd38311748
c.liebig2019 <c.liebig@campus.tu-berlin.de>
2021-05-08 00:12:31 +0200

    Instagram work in Progress

---
+++ src/main/java/dataInfoLogic/Controller/InstagramData.java
@@ -0,0 +1,56 @@
+package dataInfoLogic.Controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dataInfoLogic.DataTypes.PersonalData;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.w3c.dom.Text;
+
+import javax.swing.text.html.HTML;
+import java.io.File;
+import java.util.logging.XMLFormatter;
+
+@RestController
+public class InstagramData {
+    @CrossOrigin
+    @PostMapping(path = "/Instagram/profile_information")
+    public ResponseEntity<?> ProfileInformation(@RequestBody HTML profile) throws Exception {
+
+        PersonalData personalData = new PersonalData();
+        String htmlstring=profile.toString();
+        String htmlparts[]=htmlstring.split("\n");
+        personalData.seteMail(get_data(htmlparts,"E-Mail-Adresse"));
+
+
+        return ResponseEntity.ok(personalData);
+    }
+    //Searches in HTML-document for the given words
+    public String get_data(String[] htmlparts,String search){
+        for(int i=0;i<htmlparts.length;i++){
+            if(htmlparts[i].contains(search)){
+                while(!htmlparts[i].contains("<div>")){
+                    i++;
+                }
+                return delete_empties(htmlparts[i+1]);
+            }
+        }
+        return null;
+    }
+    //Deletes empties at the beginning of Strings
+    public String delete_empties(String string){
+        int i=0;
+        while(string.charAt(i)==' '){
+            i++;
+        }
+        return string.substring(i);
+    }
+
+}

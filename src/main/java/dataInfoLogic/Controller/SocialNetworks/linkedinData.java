c0198afac42e9f375980e71e279779434be40816
c.liebig2019 <c.liebig@campus.tu-berlin.de>
2021-05-29 17:27:54 +0200

    LinkedIn update

--- src/main/java/dataInfoLogic/Controller/SocialNetworks/linkedinData.java
+++ src/main/java/dataInfoLogic/Controller/SocialNetworks/linkedinData.java
@@ -1,8 +1,9 @@
 package dataInfoLogic.Controller.SocialNetworks;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
+import java.io.*;
+import java.nio.Buffer;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -10,46 +11,41 @@ import java.util.List;
 //import com.opencsv.exceptions.CsvException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectReader;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import java.io.IOException;
 
 @CrossOrigin
 @RestController
 public class linkedinData {
 
     @PostMapping(path = "/linkedin/Ad_Targeting")
-    public void submit(@RequestParam(value = "linkedin") MultipartFile file, ModelMap modelMap,
-                       @RequestParam(value = "uid", required = false) String uid,
-                       @RequestParam(value = "secret", required = false) String secret) throws IOException {
+    public ResponseEntity<?> submit(@RequestParam(value = "linkedin") MultipartFile file, ModelMap modelMap,
+                                 @RequestParam(value = "uid", required = false) String uid,
+                                 @RequestParam(value = "secret", required = false) String secret) throws IOException {
 
         modelMap.addAttribute("linkedin", file);
-       // String v = file.getOriginalFilename();
-        //System.out.println(v);
-       // BufferedReader br = new BufferedReader(new FileReader((File) file));
-
-
-        /*String content;
-        content = FileReader.read(file);
-        System.out.println(content); */
 
        if (!file.isEmpty()) {
-           System.out.println(file.getBytes().toString());
+           System.out.println(file.getContentType());
+           BufferedReader br;
+           List<String> result = new ArrayList<>();
+           try {
 
+               String line;
+               InputStream is = file.getInputStream();
+               br = new BufferedReader(new InputStreamReader(is));
+               while ((line = br.readLine()) != null) {
+                   result.add(line);
+               }
+               System.out.println(result);
 
-            String line = "";
-            try {
-                BufferedReader br = new BufferedReader(new FileReader((File) file));
-
-                while ((line = br.readLine()) != null) {
-                    System.out.println(line);
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-        }
+           } catch (IOException e) {
+               System.err.println(e.getMessage());
+           }
+       }
+       return null;
     }
 }
 

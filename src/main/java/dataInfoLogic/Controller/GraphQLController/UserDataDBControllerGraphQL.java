c615f561cf3d4b2b81fc709e4e4e442ed9636f24
kjell-dotcom <59034615+kjell-dotcom@users.noreply.github.com>
2021-06-24 15:44:35 +0200

    GraphQL Controller completed

--- src/main/java/dataInfoLogic/Controller/GraphQLController/UserDataDBControllerGraphQL.java
+++ src/main/java/dataInfoLogic/Controller/GraphQLController/UserDataDBControllerGraphQL.java
@@ -2,13 +2,16 @@ package dataInfoLogic.Controller.GraphQLController;
 
 import dataInfoLogic.Entities.UserData;
 import dataInfoLogic.Repositories.UserDataRepository;
+import io.leangen.graphql.annotations.GraphQLArgument;
 import io.leangen.graphql.annotations.GraphQLQuery;
 import io.leangen.graphql.spqr.spring.annotations.GraphQLApi;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 
 import java.util.List;
 
+@CrossOrigin
 @Service
 @GraphQLApi
 public class UserDataDBControllerGraphQL {
@@ -20,10 +23,18 @@ public class UserDataDBControllerGraphQL {
         this.userDataRepository = userDataRepository;
     }
 
+
     @GraphQLQuery(name = "UserData")
     public List<UserData> getAllUserData() {
 
         return (List<UserData>) userDataRepository.findAll();
+    }
+
+
+    @GraphQLQuery(name = "UserData")
+    public List<UserData> getUserData(@GraphQLArgument(name = "userId") String userId, @GraphQLArgument(name = "secret") String secret) {
+
+        return userDataRepository.getUserTopics(userId);
     }
 
 }

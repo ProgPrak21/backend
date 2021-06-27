2d49206812b972534be601e9d0fe889a521553f1
KjellBerlin <38405651+KjellBerlin@users.noreply.github.com>
2021-06-27 15:53:57 +0200

    Several structural changes

--- src/main/java/dataInfoLogic/Controller/GraphQLController/UserDataDBControllerGraphQL.java
+++ src/main/java/dataInfoLogic/Controller/GraphQLController/UserDataDBControllerGraphQL.java
@@ -1,6 +1,8 @@
 
 package dataInfoLogic.Controller.GraphQLController;
 
+import dataInfoLogic.DataTypes.FrontendDTO.UserCredentials;
+import dataInfoLogic.Services.CredentialsManager;
 import dataInfoLogic.Services.FrontEndRequests;
 import dataInfoLogic.DataTypes.DataAnalysis.TopicPercentage;
 import dataInfoLogic.Entities.UserData;
@@ -8,6 +10,9 @@ import dataInfoLogic.Repositories.UserDataRepository;
 import io.leangen.graphql.annotations.GraphQLArgument;
 import io.leangen.graphql.annotations.GraphQLQuery;
 import io.leangen.graphql.spqr.spring.annotations.GraphQLApi;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
@@ -20,33 +25,32 @@ import java.util.List;
 @GraphQLApi
 public class UserDataDBControllerGraphQL {
 
+    @Autowired
+    CredentialsManager credentialsManager;
 
-    private final UserDataRepository userDataRepository;
     private final FrontEndRequests frontEndRequests;
 
-    public UserDataDBControllerGraphQL(UserDataRepository userDataRepository, FrontEndRequests frontEndRequests) {
-        this.userDataRepository = userDataRepository;
+    public UserDataDBControllerGraphQL(FrontEndRequests frontEndRequests) {
         this.frontEndRequests = frontEndRequests;
     }
 
-
-    @GraphQLQuery(name = "UserData")
-    public List<UserData> getAllUserData() {
-
-        return (List<UserData>) userDataRepository.findAll();
-    }
-
-    //todo add secret validation
-
-
-    @GraphQLQuery(name = "UserData")
-    public List<UserData> getUserData(@GraphQLArgument(name = "userId") String userId, @GraphQLArgument(name = "secret") String secret) {
-        return userDataRepository.getUserTopics(userId);
-    }
-
-
     @GraphQLQuery(name = "UserDataAnalyzed")
     public LinkedList<TopicPercentage> getUserDataAnalyzed(@GraphQLArgument(name = "userId") String userId, @GraphQLArgument(name = "secret") String secret) {
+
+        //validate credentials, return null if wrong credentials
+        if (userId != null && secret != null) {
+
+            UserCredentials userCredentials = new UserCredentials();
+            userCredentials.setUid(userId);
+            userCredentials.setSecret(secret);
+
+            //check if user credentials are correct if they are provided
+            if (!credentialsManager.checkPw(userCredentials)) {
+                return null;
+            }
+
+        }
+
         return frontEndRequests.getUserTopicsummarized(userId);
     }
 
